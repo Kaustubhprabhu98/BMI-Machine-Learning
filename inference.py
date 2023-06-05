c29ac025c49a5d4edfcc587ae07133ede1bd475f
Kaustubh Prabhu <kaustubhprabhu98@gmail.com>
2023-06-05 08:44:33 -0400

    Update inference.py

--- inference.py
+++ inference.py
@@ -1,44 +1,96 @@
-# -*- coding: utf-8 -*-
-
-import joblib
-import os
+import boto3
 import json
+import os
+import joblib
+import pickle
+import tarfile
+import sagemaker
+from sagemaker.estimator import Estimator
+import time
+from time import gmtime, strftime
+import subprocess
 
-"""
-Deserialize fitted model
-"""
-def model_fn(model_dir):
-    model = joblib.load(os.path.join(model_dir, "model.joblib"))
-    return model
 
-"""
-input_fn
-    request_body: The body of the request sent to the model.
-    request_content_type: (string) specifies the format/variable type of the request
-"""
-def input_fn(request_body, request_content_type):
-    if request_content_type == 'application/json':
-        request_body = json.loads(request_body)
-        inpVar = request_body['Input']
-        return inpVar
-    else:
-        raise ValueError("This model only supports application/json input")
+#Setup
+client = boto3.client(service_name="sagemaker")
+runtime = boto3.client(service_name="sagemaker-runtime")
+boto_session = boto3.session.Session()
+s3 = boto_session.resource('s3')
+region = boto_session.region_name
+print(region)
+sagemaker_session = sagemaker.Session()
+role = "arn:aws:iam::258273015834:role/service-role/AmazonSageMaker-ExecutionRole-20230531T215693"
 
-"""
-predict_fn
-    input_data: returned array from input_fn above
-    model: returned model loaded from model_fn above
-"""
-def predict_fn(input_data, model):
-    return model.predict(input_data)
+#Build tar file with model data + inference code
+bashCommand = "tar -cvpzf model.tar.gz model.joblib inference.py"
+process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+output, error = process.communicate()
 
-"""
-output_fn
-    prediction: the returned value from predict_fn above
-    content_type: the content type the endpoint expects to be returned. Ex: JSON, string
-"""
+# retrieve sklearn image
+image_uri = sagemaker.image_uris.retrieve(
+    framework="sklearn",
+    region=region,
+    version="0.23-1",
+    py_version="py3",
+    instance_type="ml.m5.xlarge",
+)
 
-def output_fn(prediction, content_type):
-    res = int(prediction[0])
-    respJSON = {'Output': res}
-    return respJSON
+#Bucket for model artifacts
+default_bucket = sagemaker_session.default_bucket()
+print(default_bucket)
+
+#Upload tar.gz to bucket
+model_artifacts = f"s3://{default_bucket}/model.tar.gz"
+response = s3.meta.client.upload_file('model.tar.gz', default_bucket, 'model.tar.gz')
+
+#Step 1: Model Creation
+model_name = "bmi" + strftime("%Y-%m-%d-%H-%M-%S", gmtime())
+print("Model name: " + model_name)
+create_model_response = client.create_model(
+    ModelName=model_name,
+    Containers=[
+        {
+            "Image": image_uri,
+            "Mode": "SingleModel",
+            "ModelDataUrl": model_artifacts,
+            "Environment": {'SAGEMAKER_SUBMIT_DIRECTORY': model_artifacts,
+                           'SAGEMAKER_PROGRAM': 'inference.py'} 
+        }
+    ],
+    ExecutionRoleArn=role,
+)
+print("Model Arn: " + create_model_response["ModelArn"])
+
+
+#Step 2: EPC Creation
+sklearn_epc_name = "bmi-epc" + strftime("%Y-%m-%d-%H-%M-%S", gmtime())
+endpoint_config_response = client.create_endpoint_config(
+    EndpointConfigName=sklearn_epc_name,
+    ProductionVariants=[
+        {
+            "VariantName": "sklearnvariant",
+            "ModelName": model_name,
+            "InstanceType": "ml.c5.large",
+            "InitialInstanceCount": 1
+        },
+    ],
+)
+print("Endpoint Configuration Arn: " + endpoint_config_response["EndpointConfigArn"])
+
+
+#Step 3: EP Creation
+endpoint_name = "bmi-local-ep" + strftime("%Y-%m-%d-%H-%M-%S", gmtime())
+create_endpoint_response = client.create_endpoint(
+    EndpointName=endpoint_name,
+    EndpointConfigName=sklearn_epc_name,
+)
+print("Endpoint Arn: " + create_endpoint_response["EndpointArn"])
+
+
+#Monitor creation
+describe_endpoint_response = client.describe_endpoint(EndpointName=endpoint_name)
+while describe_endpoint_response["EndpointStatus"] == "Creating":
+    describe_endpoint_response = client.describe_endpoint(EndpointName=endpoint_name)
+    print(describe_endpoint_response["EndpointStatus"])
+    time.sleep(15)
+print(describe_endpoint_response)
